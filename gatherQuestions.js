44a053f0c6e2651f4114ba668e125bb00fd9114e
Iain MacKenzie <iainsmackenzie@gmail.com>
2021-01-10 22:59:09 -0500

    Modified gatherQuestions

--- gatherQuestions.js
+++ gatherQuestions.js
@@ -2,11 +2,11 @@ const { google } = require('googleapis');
 const sheets = google.sheets('v4');
 require('dotenv').config()
 
+const rangeToGather = 'A1:H100'
 const params = {
-    auth: process.env["GOOGLE_API_KEY"], // specify your API key here
-    // The ID of the spreadsheet to retrieve data from.
+    auth: process.env["GOOGLE_API_KEY"],
     spreadsheetId: process.env["GOOGLE_SHEET_ID"],
-    range: 'A1:H100'
+    range: rangeToGather
 }
 
 sheets.spreadsheets.values.get(params).then(({data}) => {
